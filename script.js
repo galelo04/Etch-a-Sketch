235ad8ff8f7e0ac483e83079b47dbf68ec9b16c9
Mostafa Abdelglel <oomaooka09@gmail.com>
2024-08-05 21:43:34 +0300

    feat: toggle random and add some style

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const container = document.querySelector(".container");
-const but = document.querySelector(".gridSize");
+const container = document.querySelector(".grid");
+const but = document.querySelector(".gridButtonSize");
 let arr = [];
 let colors =[];
 let square;
@@ -10,17 +10,18 @@ function makeGrid(size){
             square.className = "gridSquare";
             square.id = `square${i*size+j}`
             container.appendChild(square);
-            square.style.flexBasis = `${800/size}px`;
+            square.style.flexBasis = `${640/size}px`;
+            square.style.backgroundColor = "rgba(255,255,255,0)";
             arr.push(false);
-            colors.push("");
+            colors.push(square.style.backgroundColor);
         }
     }
 }
 container.addEventListener('mouseover' , (e)=>{
     const square = document.querySelector(`#${e.target.id}`);
+    const checkRandom = document.querySelector("input");
     if(e.ctrlKey){
         if(arr[+e.target.id.slice(6)]){
-            console.log("2");
             let currOpacity = (+colors[+e.target.id.slice(6)].slice(-4,-1)*10-0.1*10)/10;
             if(currOpacity>=0)
             colors[+e.target.id.slice(6)]= square.style.backgroundColor = colors[+e.target.id.slice(6)].slice(0,-4)+currOpacity+")";
@@ -35,10 +36,17 @@ container.addEventListener('mouseover' , (e)=>{
             colors[+e.target.id.slice(6)]= square.style.backgroundColor = colors[+e.target.id.slice(6)].slice(0,-4)+currOpacity+")";
         }
         else{
-            const redRandom = Math.floor(Math.random()*255);
-            const greenRandom = Math.floor(Math.random()*255);
-            const blueRandom = Math.floor(Math.random()*255);
-            colors[+e.target.id.slice(6)]= square.style.backgroundColor = `rgba(${redRandom},${greenRandom},${blueRandom},0.1)`;
+            let red , green ,blue;
+            if(checkRandom.checked)
+            {
+                red = Math.floor(Math.random()*255);
+                green = Math.floor(Math.random()*255);
+                blue = Math.floor(Math.random()*255);
+            }
+            else
+                red = green =blue =0;
+            
+            colors[+e.target.id.slice(6)]= square.style.backgroundColor = `rgba(${red},${green},${blue},0.1)`;
             arr[+e.target.id.slice(6)]=true;
         }
     }
@@ -48,12 +56,12 @@ container.addEventListener('mouseover' , (e)=>{
 function removeGrid() {
     const grid = document.querySelectorAll(".gridSquare");
     grid.forEach(node => node.remove());
+    arr = [];
+    colors = [];
 }
-
 
 but.addEventListener('click',()=>{
     let newSize = +prompt("Enter the size of the new grid (1~100)");
-    
     while(newSize>100 || newSize<1 || isNaN(newSize))
         newSize = +prompt("Enter the size of the new grid (1~100)");
     removeGrid();
